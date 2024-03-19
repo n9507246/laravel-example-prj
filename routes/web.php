826ca4b4318fdac37f949be0e0bcd08929169d1b
Nikita Balashov <noWorkingEmail@mail.ru>
2024-03-19 14:29:39 +0500

    создана модель Заметки, миграция и ресурс-контроллер для нее. зарегистрирован маршрут для котроллера ресурсов

--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::group(
+    [
+        'middleware' => ['auth', 'verified'], 
+        'prefix' => 'dashboard'
+    ], 
+    function () {
+        Route::get('/', function () { return view('dashboard'); })->name('dashboard');
+        Route::resource('notes', \App\Http\Controllers\NoteController::class);
+    }
+);
+
 
 require __DIR__.'/auth.php';
