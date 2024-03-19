ece9580a5c43ad4ed4303727b207fc1aca8a3bfb
Nikita Balashov <noWorkingEmail@mail.ru>
2024-03-19 15:18:40 +0500

    в представлении для создания заметки добавлена форма для создания заметки

---
+++ resources/views/components/textarea-input.blade.php
@@ -0,0 +1,11 @@
+@props(['rows' => 5,  'cols'=> 20])
+
+<textarea
+    {!! $attributes->merge(
+        ['class' => 'mt-1 block w-full 
+            border-gray-300 focus:border-indigo-500 
+            focus:ring-indigo-500 rounded-md shadow-sm',
+            'rows' => $rows, 'cols' => $cols
+        ]) 
+    !!}
+></textarea>
--- resources/views/notes/create.blade.php
+++ resources/views/notes/create.blade.php
@@ -8,8 +8,29 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("Create note") }}
+                <div class="p-6 text-gray-900"> 
+                    <form method="post" action="{{ route('notes.store') }}" class="mt-6 space-y-6">
+                        @csrf
+                        <div>
+                            <x-input-label for="title" :value="__('Title')" />
+                            <x-text-input id="title" name="title" type="text" class="mt-1 block w-full" autocomplete="title" />
+                            <x-input-error :messages="$errors->get('title')" class="mt-2" />
+                        </div>
+
+
+                        <div>
+                            <x-input-label for="body" :value="__('Body')" />
+                            <x-textarea-input id="body" name="body" class="mt-1 block w-full">
+                                {{old('body')}}
+                            </x-textarea-input>
+                            <x-input-error :messages="$errors->get('body')" class="mt-2" />
+                        </div>
+                
+                
+                        <div class="flex items-center gap-4">
+                            <x-primary-button>{{ __('Create') }}</x-primary-button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
